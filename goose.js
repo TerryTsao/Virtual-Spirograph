7598f88bf08cbcfd81395e7b0127b59f0edd4776
TerryTsao <xxc142@case.edu>
2017-07-27 18:27:39 -0400

    Satisfied as a prototype.
    * goose.html cannot use for now. XMLHttpRequest chrome not supported.
    * paper.js version

--- goose.js
+++ goose.js
@@ -1,3 +1,13 @@
+var path = new Path();
+	// Give the stroke a color
+	path.strokeColor = 'black';
+	var start = new Point(100, 100);
+	// Move to start and draw a line from there
+	path.moveTo(start);
+	// Note the plus operator on Point objects.
+	// PaperScript does that for us, and much more!
+	path.lineTo(start + [ 100, -50 ]);
+/*
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -82,7 +92,7 @@ function setAngle(flakeAngle) {
     });
     
     canvas.add(newDot);
-    */
+
 }
 
 setAngle(Math.PI);
